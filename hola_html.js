62bdf71e0230b0e15136410bca1a562d04c07e98
jesus lopez <jesus.lopez@venalum.com.ve>
2018-11-29 15:23:45 -0400

    4.-EncabezadosyObjetoResponse

--- hola_html.js
+++ hola_html.js
@@ -3,6 +3,9 @@ var fs = require("fs")
 
 http.createServer(function(req, res){
    fs.readFile("./index.html", function(err,html){
+        res.wirteHead(200,{
+            "Content-Type":"text/html"
+        })
         res.write(html)
         res.end()
    })
