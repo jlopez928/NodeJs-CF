92e822e3f4350e55bf10de7ab97ce5b8b9338838
jesus lopez <jesus.lopez@venalum.com.ve>
2018-12-03 11:18:20 -0400

    10.-MotorVistaJADE

--- hola_express/app.js
+++ hola_express/app.js
@@ -2,8 +2,11 @@ var express = require("express")
 
 var app = express()
 
+app.set("view engine","jade")
+
 app.get("/", function(req,res) {
-    res.send("Hola Mundo")
+    //res.send("Hola Mundo")
+    res.render("index", {hola: "Hola Jesus Lopez"})
 })
 
 app.listen(3000, function() {
